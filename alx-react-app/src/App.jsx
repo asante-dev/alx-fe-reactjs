0282a364430ac190b7d00b3106296c4b27ce2e88
ChaseDev <clintonkwabena@gmail.com>
2025-07-14 09:59:40 -0700

    Add UserProfile in App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,11 +6,19 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
+
+
+
+
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <UserProfile name="John Doe" age={30} bio="Loves traveling and photography." />
       <Header />
       <MainContent />
       <Footer />
