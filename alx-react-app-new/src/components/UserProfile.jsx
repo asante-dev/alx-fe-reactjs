6e1a1e996d4215b59466b02853e77a6d14095976
ChaseDev <clintonkwabena@gmail.com>
2025-07-16 17:17:35 +0000

    Update styling in UserProfile.jsx

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -2,22 +2,31 @@ const UserProfile = (props) => {
 const h2Style = {
   color: 'blue',
   fontWeight: 'bold',
+
 };
 
 const ageStyle = {
   color: 'green',
   fontStyle: 'italic',
+  margin: '10px 0',
 };
 
 const bioStyle = {
   color: 'gray',
   fontStyle: 'italic',
+  border : '1px solid #ccc',
+  padding: '10px',
+};
+
+const ageNumberStyle = {
+  color: 'red',
+  fontWeight: 'bold',
 };
 
   return (
     <div>
       <h2 style={h2Style}>{props.name}</h2>
-      <p style={ageStyle}>Age: {props.age}</p>
+      <p style={ageStyle}>Age: <span style={ageNumberStyle}>{props.age}</span></p>
       <p style={bioStyle}>Bio: {props.bio}</p>
      </div>
    );
