563261c0aa60682a20e72343ad22ff7c82763eae
ChaseDev <clintonkwabena@gmail.com>
2025-07-19 19:09:20 +0000

    Add inline

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,13 +1,39 @@
 import { Link } from "react-router-dom";
 
+const NavbarStyles = {
+  nav: { 
+    justifyContent: "center", 
+    padding: "1rem", 
+    background: "black", 
+    display: "flex", 
+  },
+
+  ul: { 
+    listStyle: "none",
+    display: "flex",
+    gap: "1rem",
+    color: "white",
+    textColor: "white"
+  },
+  li: { 
+    color: "white", 
+  },
+
+  Link: {
+    textDecoration: "none",
+    color: "white",
+   
+  }
+};
+
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/services">Services</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+    <nav style={NavbarStyles.nav}>
+      <ul style={NavbarStyles.ul}>
+        <li style={NavbarStyles.li}><Link style={NavbarStyles.Link} to="/">Home</Link></li>
+        <li style={NavbarStyles.li}><Link style={NavbarStyles.Link} to="/about">About</Link></li>
+        <li style={NavbarStyles.li}><Link style={NavbarStyles.Link} to="/services">Services</Link></li>
+        <li style={NavbarStyles.li}><Link style={NavbarStyles.Link} to="/contact">Contact</Link></li>
       </ul>
     </nav>
   );
