08de43a24e49092ce0cb90bcb3dc6c7964560595
ChaseDev <clintonkwabena@gmail.com>
2025-08-01 12:13:55 +0000

    Add routes in App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,15 +1,26 @@
+import { Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import SearchBar from './components/SearchBar';
+import RecipeDetails from './components/RecipeDetails';
 
 function App() {
-
   return (
     <div>
       <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <RecipeList />
-      <SearchBar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <AddRecipeForm />
+              <SearchBar />
+              <RecipeList />
+            </>
+          }
+        />
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+      </Routes>
     </div>
   );
 }
--- recipe-sharing-app/src/main.jsx
+++ recipe-sharing-app/src/main.jsx
@@ -8,10 +8,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/recipe/:id" element={<RecipeDetails />} />
-      </Routes>
+     <App />
     </BrowserRouter>
   </React.StrictMode>
 );
