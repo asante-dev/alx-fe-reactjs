3647ca91b2cb74ee03b75d91dd1b55c37950c9e8
ChaseDev <clintonkwabena@gmail.com>
2025-08-01 10:07:58 +0000

    Create Recipe adding app

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,16 @@
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+import { useRecipeStore } from './RecipeStore';
+
+function App() {
+
+  return (
+    <div>
+      <h1>Recipe Sharing App</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
+}
+
+export default App;
