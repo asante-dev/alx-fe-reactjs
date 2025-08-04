fdbdd10f03ad76263c598b2a29c845ed54536604
ChaseDev <clintonkwabena@gmail.com>
2025-08-04 00:30:56 +0000

    Integrate the guthub API and create a basic search feature

---
+++ github-user-search/src/services/githubService.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://api.github.com/users/';
+
+async function fetchUserData(username) {
+  const response = await axios.get(`${BASE_URL}${username}`);
+  return response.data;
+}
+
+export default fetchUserData;
