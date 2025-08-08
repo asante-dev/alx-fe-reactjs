189e6828733646db57a002dbaec470a82c336665
ChaseDev <clintonkwabena@gmail.com>
2025-08-08 13:13:34 +0000

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,10 +1,35 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://api.github.com/users/';
+export const fetchUserData = async (username) => {
+  try {
+    const response = await axios.get(`https://api.github.com/users/${username}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+import axios from "axios";
 
-async function fetchUserData(username) {
-  const response = await axios.get(`${BASE_URL}${username}`);
-  return response.data;
+export async function fetchGitHubUsers({ username, location, minRepos }) {
+  let query = "";
+
+  if (username) query += `${username} in:login `;
+  if (location) query += `location:${location} `;
+  if (minRepos) query += `repos:>=${minRepos} `;
+
+  const url = `https://api.github.com/search/users?q=${encodeURIComponent(
+    query.trim()
+  )}`;
+
+  const response = await axios.get(url);
+
+  // Enrich user data with additional details (since GitHub search API doesn’t return all fields)
+  const detailedUsers = await Promise.all(
+    response.data.items.slice(0, 10).map(async (user) => {
+      const detail = await axios.get(user.url);
+      return detail.data;
+    })
+  );
+
+  return detailedUsers;
 }
-
-export default fetchUserData;
