b5fe93393c106ae1d041865eca334dedef8e6a99
ChaseDev <clintonkwabena@gmail.com>
2025-08-23 19:17:14 +0000

    Put react query setup in App.jsx component

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,12 +1,19 @@
 // App.jsx
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import PostsComponent from "./PostsComponent";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-2xl font-bold p-4">React Query Demo</h1>
-      <PostsComponent />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1 className="text-2xl font-bold p-4">React Query Demo</h1>
+        <PostsComponent />
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
